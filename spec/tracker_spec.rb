4c1327aff3282d173ed1d4e28a561365ad04f2ac
André Taiar <andre.taiar@appprova.com.br>
2016-04-26 14:31:39 -0300

    Dry'ing specs.

--- spec/tracker_spec.rb
+++ spec/tracker_spec.rb
@@ -5,36 +5,32 @@ include ActionTracker::Helpers::Render
 describe ActionTracker::Concerns::Tracker do
 
   before(:each) do
+    @event_list = []
     @fake_session = {}
     allow_any_instance_of(ApplicationTestController).to receive(:session).and_return(@fake_session)
     @helper = Object.new.extend ActionTracker::Helpers::Render
   end
 
-  let(:kontroller_klass) { ApplicationTestController.new { include ActionTracker::Concerns::Tracker  } }
-  let(:second_kontroller_klass) { ApplicationTestController.new { include ActionTracker::Concerns::Tracker } }
-
   it 'track an action' do
-    allow(kontroller_klass).to receive(:action_name).and_return('action_test')
-    kontroller_klass.track_event
-    expect(kontroller_klass.session[:action_tracker]).to eq(['Here comes the test'])
+    trigger_trackers(['action_test'])
+    expect(@fake_session[:action_tracker]).to eq(['Here comes the test'])
   end
 
   it 'return null if class does not have tracker' do
-    allow(kontroller_klass).to receive(:action_name).and_return('some_other_test_action')
-    kontroller_klass.track_event
-    expect(kontroller_klass.session[:action_tracker]).to be_empty
+    trigger_trackers(['some_other_test_action'])
+    expect(@fake_session[:action_tracker]).to be_empty
   end
 
   it 'stacks sequence of trackers' do
-    trigger_two_trackers
-    expect(kontroller_klass.session[:action_tracker].size).to eq(2)
-    expect(kontroller_klass.session[:action_tracker].first).to eq('Here comes the test')
-    expect(kontroller_klass.session[:action_tracker].last).to eq(action: 'Here comes the object test')
+    trigger_trackers(['action_test', 'another_action_test'])
+    expect(@fake_session[:action_tracker].size).to eq(2)
+    expect(@fake_session[:action_tracker].first).to eq('Here comes the test')
+    expect(@fake_session[:action_tracker].last).to eq(action: 'Here comes the object test')
   end
 
   it 'clear stacks sequence of trackers when the helper is called' do
-    trigger_two_trackers
-    expect(kontroller_klass.session[:action_tracker].size).to eq(2)
+    trigger_trackers(['action_test', 'another_action_test'])
+    expect(@fake_session[:action_tracker].size).to eq(2)
     allow(@helper).to receive(:session).and_return(@fake_session)
     result = @helper.track_event
     expect(@fake_session[:action_tracker]).to be_nil
@@ -42,11 +38,12 @@ describe ActionTracker::Concerns::Tracker do
     expect(result).to include('Here comes the test', action: 'Here comes the object test')
   end
 
-  def trigger_two_trackers
-    allow(kontroller_klass).to receive(:action_name).and_return('action_test')
-    allow(second_kontroller_klass).to receive(:action_name).and_return('another_action_test')
-    kontroller_klass.track_event
-    second_kontroller_klass.track_event
+  def trigger_trackers(event_list)
+    event_list.size.times do |n|
+      @event_list[n] = ApplicationTestController.new { include ActionTracker::Concerns::Tracker  }
+      allow(@event_list[n]).to receive(:action_name).and_return(event_list[n])
+      @event_list[n].track_event
+    end
   end
 
 end
