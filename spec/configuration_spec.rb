d336bff93d07177bc3785ec0e058814e7566c0f9
Tiago Martins Araújo <tiago.martins.araujo@gmail.com>
2016-05-02 14:02:35 -0300

    improving tests

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -1,17 +1,27 @@
 require 'spec_helper'
 
 describe ActionTracker::Configuration do
-  describe '#track_events flag' do
+  describe '#track_events' do
     it 'default value is true' do
-      expect(ActionTracker::Configuration.new.track_events).to eq(true)
+      expect(ActionTracker.configuration.track_events).to eq(true)
     end
   end
 
-  describe '#track_events flag=' do
-    it 'can set value' do
-      config = ActionTracker::Configuration.new
-      config.track_events = false
-      expect(config.track_events).to be_falsy
+  context '#track_events false' do
+    before(:each) do
+      ActionTracker.configure do |config|
+        config.track_events = false
+      end
+    end
+
+    after(:each) do
+      ActionTracker.configure do |config|
+        config.track_events = true
+      end
+    end
+
+    it 'can be setted to false' do
+      expect(ActionTracker.configuration.track_events).to eq(false)
     end
   end
 end
