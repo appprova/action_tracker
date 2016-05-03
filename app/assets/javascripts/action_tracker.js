4fbf42b8039722b6a78db7f6dbcbf879f2fd5801
André Taiar <andre.taiar@appprova.com.br>
2016-05-03 14:48:29 -0300

    Refactoring callbacks.

--- app/assets/javascripts/action_tracker.js
+++ app/assets/javascripts/action_tracker.js
@@ -3,21 +3,26 @@
 
 var ActionTracker = function () {
 
-  var storage;
+  var storage,
+      callbacks = {};
 
   function constructor() {
     storage = new Storage();
   }
 
+  function setCallbacks(callbacksObj) {
+    callbacks = callbacksObj;
+  }
+
   function start(list) {
     storage.queue(list);
-    process();
   }
 
   function process() {
-    while(storage.getFirst() !== undefined) {
+    while(typeof storage.getFirst() !== 'undefined') {
       var tracker = new Tracker(storage.getFirst());
       tracker.send();
+      storage.dequeue();
     }
   }
 
@@ -30,22 +35,24 @@ var ActionTracker = function () {
         this.getStorage();
       else
         this.setStorage();
-    }
+    };
 
     this.getStorage = function() {
       this.storage = JSON.parse(sessionStorage.getItem('action_tracker_storage'));
-    }
+    };
 
     this.setStorage = function() {
       sessionStorage.setItem('action_tracker_storage', JSON.stringify(this.storage));
-    }
+    };
 
     this.queue = function(list) {
-      this.getStorage();
-      for (var i =  0; i < list.length; i++)
-        this.storage.push(list[i]);
-      this.setStorage();
-    }
+      if(list != null) {
+        this.getStorage();
+        for (var i =  0; i < list.length; i++)
+          this.storage.push(list[i]);
+        this.setStorage();
+      }
+    };
 
     this.dequeue = function() {
       this.getStorage();
@@ -53,56 +60,59 @@ var ActionTracker = function () {
       this.storage.splice(0, 1);
       this.setStorage();
       return first;
-    }
+    };
 
     this.getFirst = function() {
       this.getStorage();
       return this.storage[0];
-    }
+    };
 
     this.constructor();
   };
 
-  function Tracker() {
+  function Tracker(tracker_data) {
+
+    this.userFlag = false;
+    this.dataFlag = false;
+    this.user = null;
+    this.data = null;
+
+    if(typeof tracker_data !== 'undefined') {
+      if(typeof tracker_data.identify !== 'undefined') {
+        this.userFlag = true;
+        this.user = new User(tracker_data.identify);
+      }
+      if(typeof tracker_data.track !== 'undefined') {
+        this.dataFlag = true;
+        this.data = tracker_data.track;
+      }
+    }
+
+    this.send = function() {
+      if(this.userFlag) callbacks.identify(this.user.getData());
+      if(this.dataFlag) callbacks.track(this.data);
+    };
   };
 
   function User(user_data) {
     this.data = user_data;
-    this.data.id = dito.generateID(this.data.email);
+    this.data.id = callbacks.generateID(this.data.email);
+
+    this.getData = function() {
+      return this.data;
+    };
   };
 
   var publico = {
     Tracker: Tracker,
     User: User,
     Storage: Storage,
-    new: start
+    new: start,
+    push: process,
+    callbacks: setCallbacks
   };
 
   constructor();
 
   return publico;
-}();
-
-var dito = function() {
-
-  var publico = {
-    generateID: generateID,
-    identify: identify,
-    track: track
-  };
-
-  function generateID() {
-    return 'oi gente';
-  };
-
-  function identify() {
-    console.log('identificou isso')
-  };
-
-  function track() {
-    console.log('trackeou isso')
-  };
-
-  return publico;
-
 }();
