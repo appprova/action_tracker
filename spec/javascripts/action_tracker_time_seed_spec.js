bf3bf6ba4486a40741b80a186791cfec303d3db7
André Taiar <andre.taiar@appprova.com.br>
2016-05-24 08:54:01 -0300

    Time seed spec.

---
+++ spec/javascripts/action_tracker_time_seed_spec.js
@@ -0,0 +1,18 @@
+
+var ActionTracker = ActionTracker || {};
+
+describe('Action Tracker Time Seed', function() {
+
+  beforeEach(function() {
+    timeSeed = new ActionTracker.TimeSeed();
+  });
+
+  it('increase time by 1 sec every time it gets called', function(){
+    var second = timeSeed.getTimeSeed().getSeconds();
+    expect(timeSeed.getTimeSeed().getSeconds() - second).toEqual(1);
+    expect(timeSeed.getTimeSeed().getSeconds() - second).toEqual(2);
+    expect(timeSeed.getTimeSeed().getSeconds() - second).toEqual(3);
+    expect(timeSeed.getTimeSeed().getSeconds() - second).toEqual(4);
+  });
+
+});
