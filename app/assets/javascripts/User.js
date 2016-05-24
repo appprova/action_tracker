16dbf3673e040aff6477ed531fe1fd9214159d85
André Taiar <andre.taiar@appprova.com.br>
2016-05-24 09:53:25 -0300

    User specs.

--- app/assets/javascripts/User.js
+++ app/assets/javascripts/User.js
@@ -1,15 +1,22 @@
 var ActionTracker = (function(self) {
 
-  self.User = function(userData, callbacks) {
-    var data = userData;
+  self.User = function(userData, userCallbacks) {
+    var      data = userData,
+        callbacks = userCallbacks;
+
     data.id  = callbacks.generateID(data.email);
 
     function getData() {
       return data;
     }
 
+    function getCallbacks() {
+      return callbacks;
+    }
+
     return {
-      getData: getData
+      getData: getData,
+      getCallbacks: getCallbacks
     };
   };
 
